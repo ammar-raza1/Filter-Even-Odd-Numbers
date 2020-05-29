ed739afcd0c3fbd7775e99ca2e22f47302e58ade
ammar-raza1 <59652259+ammar-raza1@users.noreply.github.com>
2020-05-29 18:03:04 +0500

    Update BaseUtil.java

--- src/main/java/com/tenpearls/filteringevenodd/Utils/BaseUtil.java
+++ src/main/java/com/tenpearls/filteringevenodd/Utils/BaseUtil.java
@@ -5,4 +5,7 @@ public class BaseUtil {
     public static Boolean checkIfEven(Integer number) {
         return number % 2 == 0;
     }
+    public static Boolean checkIfOdd(Integer number) {
+        return number % 2 != 0;
+    }
 }
